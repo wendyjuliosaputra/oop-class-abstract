0b15e81bc68ba76e434ebd00aef8c69fb856408e
wendyjuliosaputra <124264447+wendyjuliosaputra@users.noreply.github.com>
2023-03-15 13:04:01 +0700

    Add files via upload

---
+++ V26.cs
@@ -0,0 +1,18 @@
+namespace V26
+{
+      abstract  class Kalkulator
+    {
+      public  int kali (int a, int b){
+        return a * b;
+      }
+    }
+
+    class Rumus:Kalkulator
+    {
+       public int persegi (int p, int l){
+        return kali(p,l);
+       }
+    }
+    
+    
+}
